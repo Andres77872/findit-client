db6eb364a23d8dd7e48155ff7c7d55a968d7d02f
andres77872 <andreslamosk124@hotmail.com>
2023-10-28 20:04:23 -0600

    Add image validation and improve booru_name handling
    Implemented validation for loading multiple images. In the previous version, the 'booru_name' parameter was checked without confirming its existence which could lead to errors. The changes ensure it exists before the check.

--- findit_client/util/validations.py
+++ findit_client/util/validations.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from findit_client.api.const import BOORUS_NAMES_STR, BOORU_TO_ID
 from findit_client.exceptions import SearchBooruNotFound
 
@@ -20,9 +22,10 @@ def validate_params(func):
                 kwargs['pool'] = BOORUS_NAMES_STR
 
         if 'booru_name' in kwargs:
-            if kwargs['booru_name'] not in BOORUS_NAMES_STR:
-                raise SearchBooruNotFound(booru=kwargs['booru_name'])
-            kwargs['booru_name'] = BOORU_TO_ID[kwargs['booru_name']]
+            if kwargs['booru_name']:
+                if kwargs['booru_name'] not in BOORUS_NAMES_STR:
+                    raise SearchBooruNotFound(booru=kwargs['booru_name'])
+                kwargs['booru_name'] = BOORU_TO_ID[kwargs['booru_name']]
 
         if 'pool_vector' in kwargs:
             if kwargs['pool_vector'] not in BOORUS_NAMES_STR:
@@ -30,5 +33,19 @@ def validate_params(func):
             kwargs['pool_vector'] = BOORU_TO_ID[kwargs['pool_vector']]
         num_sum = func(*args, **kwargs)
         return num_sum
+
+    return wrapper
+
+
+def validate_load_image(func):
+    def wrapper(image: str | list[str], *args, **kwargs):
+        if isinstance(image, str):
+            res = func(image, *args, **kwargs)
+        else:
+            res = [func(x, *args, **kwargs) for x in image]
+            tm = sum([x[1] for x in res])
+            np_image = np.concatenate(np.array([x[0] for x in res]), axis=0, dtype=res[0][0].dtype)
+            res = np_image, tm
+        return res
 
     return wrapper
