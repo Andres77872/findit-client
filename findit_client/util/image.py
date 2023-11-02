451e152395f2eab1ca68f04cfbd7be64d815ccc2
andres77872 <andreslamosk124@hotmail.com>
2023-11-02 17:30:14 -0600

    msaonry png force

--- findit_client/util/image.py
+++ findit_client/util/image.py
@@ -99,7 +99,7 @@ def build_masonry_collage(results: ImageSearchResponseModel) -> tuple[np.ndarray
         return cv2.resize(img, (w, h)), h
 
     def run(url):
-        rq = sess.get(url, timeout=5, stream=True)
+        rq = sess.get(url+'.png', timeout=5, stream=True)
         arr = np.asarray(bytearray(rq.content), dtype=np.uint8)
         img = cv2.imdecode(arr, -1)
 
