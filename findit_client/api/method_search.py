ab6a9f2447af2451221b65962b4ed5da7d722993
andres77872 <andreslamosk124@hotmail.com>
2023-11-16 11:32:29 -0600

    "Added 'rating' parameter to search methods
    A 'rating' parameter is introduced into each of the search methods in the 'findit_client' file to refine the search results. Users can now filter the search results based on ratings. This was provided as a part of improving the search functionality and user experience."

--- findit_client/api/method_search.py
+++ findit_client/api/method_search.py
@@ -18,13 +18,15 @@ class FindItMethodsSearch:
             self,
             img: str | list[str],
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         img_array, tm = load_file_image(img)
         return self.ApiRequests.search_by_ndarray_image_input(
             img_array=img_array,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='FILE',
             load_image_time=tm,
             api_version=self.__version__
@@ -34,13 +36,15 @@ class FindItMethodsSearch:
             self,
             url: str,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         img_array, tm = load_url_image(url)
         return self.ApiRequests.search_by_ndarray_image_input(
             img_array=img_array,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='URL',
             load_image_time=tm,
             api_version=self.__version__
@@ -50,13 +54,15 @@ class FindItMethodsSearch:
             self,
             img: bytes,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         img_array, tm = load_bytes_image(img)
         return self.ApiRequests.search_by_ndarray_image_input(
             img_array=img_array,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='BIN_FILE',
             load_image_time=tm,
             api_version=self.__version__
@@ -67,13 +73,15 @@ class FindItMethodsSearch:
             booru_name: str,
             image_id: int,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         return self.ApiRequests.search_by_booru_image_id(
             id_vector=image_id,
             pool_vector=booru_name,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='QUERY',
             load_image_time=0,
             api_version=self.__version__
@@ -83,7 +91,8 @@ class FindItMethodsSearch:
             self,
             query: str,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         dec, _ = arzypher_decoder(**X_query_arzypher_params,
                                   encoded=query)
@@ -96,6 +105,7 @@ class FindItMethodsSearch:
             pool_vector=ID_TO_BOORU[booru_id],
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='QUERY',
             load_image_time=0,
             api_version=self.__version__
@@ -105,12 +115,14 @@ class FindItMethodsSearch:
             self,
             vector: list,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         return self.ApiRequests.search_by_vector_input(
             vector=vector,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='VECTOR',
             load_image_time=0,
             api_version=self.__version__
@@ -120,13 +132,15 @@ class FindItMethodsSearch:
             self,
             text: str,
             pool: list[str] = None,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         return self.ApiRequests.search_by_string(
             use_sem=True,
             text=text,
             pool=pool,
             limit=limit,
+            rating=rating,
             mode='TEXT',
             load_image_time=0,
             api_version=self.__version__
@@ -135,10 +149,12 @@ class FindItMethodsSearch:
     def scroll(
             self,
             scroll_token: str,
-            limit: int = 32
+            limit: int = 32,
+            rating: list[str] = None
     ) -> ImageSearchResponseModel:
         return self.ApiRequests.search_scroll(
             limit=limit,
+            rating=rating,
             scroll_token=scroll_token,
             mode='SCROLL',
             load_image_time=0,
